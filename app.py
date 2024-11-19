18b57bd449f6320b88c9f33d0869ec079c64575d
mclint <mclint@uw.edu>
2024-11-19 11:07:43 -0800

    changed app.py and worked with adding API keys

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-
+import keys
 app = Flask(__name__)
 @app.route("/")
 def index():
+
     fruits = [
         {"name": "apples", "quantity": 3},
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
-    return render_template("index.html", fruits=fruits)
+
+    filtered_fruits = [fruit for fruit in fruits if fruit["name"].startswith("o") and fruit["quantity"] < 3]
+    return render_template("index.html", fruits=filtered_fruits, key_1=keys.MY_SECRET_API_KEY)
